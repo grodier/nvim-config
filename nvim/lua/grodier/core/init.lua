e6b69185d6ccf801735f7ef0bc9910ed029c9b1b
grodier <grodier3@gmail.com>
2025-01-21 22:42:30 -0500

    2025 refresh

--- nvim/lua/grodier/core/init.lua
+++
@@ -1,2 +0,0 @@
-require("grodier.core.options")
-require("grodier.core.keymaps")
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,4 +1,13 @@
-vim.g.mapleader = " "
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set ai")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set cursorline")
+
+vim.g.mapleader= " "
 
 local keymap = vim.keymap
 
